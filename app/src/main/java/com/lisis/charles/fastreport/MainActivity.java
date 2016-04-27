d05e44fef5df12c267ccb41fdf8cf035d1811b2f
Lis <lisrod01@gmail.com>
2016-04-27 21:22:59 +0200

    DB

--- app/src/main/java/com/lisis/charles/fastreport/MainActivity.java
+++ app/src/main/java/com/lisis/charles/fastreport/MainActivity.java
@@ -2,6 +2,7 @@ package com.lisis.charles.fastreport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import R
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/db/FastReportDataBase.java
+++ app/src/main/java/db/FastReportDataBase.java
@@ -2,6 +2,7 @@ package db;
 import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.R;
 /**
  * Created by Lis on 27/4/16.
  */
@@ -11,7 +12,9 @@ public class FastReportDataBase extends Activity{
         public void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
+            setContentView(R.layout.);
+            //solve this error importing: import your.application.packagename.R;
+
 
             //Abrimos la base de datos 'UsersDB' en modo escritura
             SQLiteHelper usdbh =
@@ -22,17 +25,13 @@ public class FastReportDataBase extends Activity{
             //Si hemos abierto correctamente la base de datos
             if(db != null)
             {
-                //Insertamos 5 usuarios de ejemplo
-                for(int i=1; i<=5; i++)
-                {
-                    //Generamos los datos
-                    int codigo = i;
-                    String nombre = "Usuario" + i;
-
-                    //Insertamos los datos en la tabla Usuarios
-                    db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
+                int codigo = 12;
+                string nombre = "Jorge Valdes";
+                //Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
                             "VALUES (" + codigo + ", '" + nombre +"')");
-                }
+
+            }
 
                 //Cerramos la base de datos
                 db.close();
